5e17ca5e65c868c60e82c3ab3ce2ea4a7e5e7b52
Bryce Penberthy <bryce@servolabs.com>
2012-10-21 09:28:51 -0500

    Fixing Emma coverage reporting

--- thomas-android/src/test/java/com/servolabs/thomas/dummy/DummyContentTest.java
+++ thomas-android/src/test/java/com/servolabs/thomas/dummy/DummyContentTest.java
@@ -4,11 +4,15 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import com.servolabs.robolectric.ThomasTestRunner;
+
+@RunWith(ThomasTestRunner.class)
 public class DummyContentTest {
-	
+
 	@Test
-	public void makeThreeDummyItems()  {
+	public void makeThreeDummyItems() {
 		assertThat(DummyContent.ITEMS.size(), is(3));
 	}
 
